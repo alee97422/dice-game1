cab26cb4f30e6f6577b712aab1d044517acf0b18
alee97422 <alee97422@gmail.com>
2023-10-20 19:43:59 -0500

    updated pydice.py to be a complete CLI game instead of having to run everytime

--- pydice.py
+++ pydice.py
@@ -1,18 +1,22 @@
 import random
-#this is a simple sex dice game that is #usable with 2+ people
-Body_Parts = ["lips", "genitals","thigh","neck", "chest","foot"]
-Acts = ["suck", "lick", "nibble","caress","kiss","massage"]
-Players = ["player1", "player2", "player3"]
+
+Body_Parts = ["lips", "genitals", "thigh", "neck", "chest", "foot"]
+Acts = ["suck", "lick", "nibble", "caress", "kiss", "massage"]
+
+# Ask for the number of players
+num_players = int(input("Enter the number of players: "))
+Players = [input(f"Enter name for player {i + 1}: ") for i in range(num_players)]
 
 def Roll1():
-  #i would like to simplify this code please work on yourself 
-  rp2 = random.sample(Players, 2)
-  Roll_body = random.choice(Body_Parts)
-  Roll_act = random.choice(Acts)
-  print (rp2)
-  print (Roll_act)
-  print (Roll_body)
-  
-Roll1()
+    rp2 = random.sample(Players, 2)
+    Roll_body, Roll_act = random.choice(Body_Parts), random.choice(Acts)
+    print(f"{rp2}\n{Roll_act}\n{Roll_body}")
 
-
+# Create a loop that keeps asking for user input until they decide to stop
+while True:
+    user_input = input("Press Enter to roll (or type 'exit' to quit): ")
+    
+    if user_input.lower() == "exit":
+        break  # Exit the loop if the user types 'exit'
+    
+    Roll1()
